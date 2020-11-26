814682a988653aa11113c8cab09519f6e797f590
patFish <fischer.pa7rick@gmail.com>
2020-11-26 09:24:54 +0100

    FEAT: Testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Team</p>
+        <p>Hello Team -- Testing</p>
         <a
           className="App-link"
           href="https://reactjs.org"
