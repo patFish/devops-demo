cfd6ab607717778c34d49a8706b95981298161bf
patFish <fischer.pa7rick@gmail.com>
2020-11-26 09:36:32 +0100

    tech meeting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Team -- Testing</p>
+        <p>Hello Team -- Hello Tech Meeting</p>
         <a
           className="App-link"
           href="https://reactjs.org"
